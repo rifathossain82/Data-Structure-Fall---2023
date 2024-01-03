381c3f2c7b5961ccb6c6ccf3790e185900143ddf
rifathossain82 <officialrifat82@gmail.com>
2024-01-03 08:31:48 +0600

    Fibonacci number problem solved

---
+++ src/practice/fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int fibonacci(int n){
+    if(n <= 1){
+        return n;
+    }
+
+    return fibonacci(n - 1) + fibonacci (n - 2);
+}
+
+int main(){
+    for(int i = 0; i < 10; i++){
+        printf("%d\t", fibonacci(i));
+    }
+
+    return 0;
+}
